7b7ef6215534f08162dfc46d32af845196157719
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-22 19:43:31 +0200

    add mongoose and connect to the db

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
+
+mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
@@ -9,6 +12,28 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
   res.render('landing');
 });
+
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image:String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+/* Campground.create({
+  
+    name: 'Granite Hill',
+    image: 'https://images.unsplash.com/photo-1503789597747-41de608aca69?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1e39a51d6a2893ca49d551ad22e86ccc&auto=format&fit=crop&w=500&q=60'
+  
+  }, function (err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('New Campground Created');
+      console.log(campground);
+    }
+  }); */
 
 var campgrounds = [
   { name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec456c4aeb71d3aecbe65e586d186ec0&auto=format&fit=crop&w=500&q=60' },
@@ -23,16 +48,30 @@ var campgrounds = [
 ];
 
 app.get('/campgrounds', function(req, res) {
-  
-  res.render('campgrounds', {campgrounds: campgrounds});
+  //get campgrounds from the db
+  Campground.find({}, function (err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('campgrounds', {campgrounds: allCampgrounds});
+    }
+  });
+ 
 });
 
 app.post('/campgrounds', function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  res.redirect('/campgrounds');
+  //Create a new campground and save to the db
+  Campground.create(newCampground, function (err, newCreated) {
+     if (err) {
+       console.log(err);
+     } else{
+      res.redirect('/campgrounds');
+     }
+  });
+
 });
 
 app.get('/campgrounds/new', function (req, res) {
