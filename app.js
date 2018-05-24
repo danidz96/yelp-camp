703f3ad7a52a9ed3bf7d9e70b39e7f75c90c7386
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-24 19:56:51 +0200

    add seed and comments

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
-    Campground = require('./models/campground');
+    mongoose = require('mongoose'),
+    Campground = require('./models/campground'),
+    seedDB = require('./seeds');
 
-
+seedDB();
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -14,33 +15,6 @@ app.get('/', function(req, res) {
   res.render('landing');
 });
 
-
-
-/* Campground.create({
-  
-    name: 'Granite Hill',
-    image: 'https://images.unsplash.com/photo-1503789597747-41de608aca69?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1e39a51d6a2893ca49d551ad22e86ccc&auto=format&fit=crop&w=500&q=60',
-    description: 'This is a huge granite hill, no bathrooms. No water. Beautiful granite!'
-  }, function (err, campground) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('New Campground Created');
-      console.log(campground);
-    }
-  }); */
-
-var campgrounds = [
-  { name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec456c4aeb71d3aecbe65e586d186ec0&auto=format&fit=crop&w=500&q=60' },
-  { name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1503789597747-41de608aca69?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1e39a51d6a2893ca49d551ad22e86ccc&auto=format&fit=crop&w=500&q=60' },
-  { name: 'High Mountain', image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=500&q=60' },
-  { name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec456c4aeb71d3aecbe65e586d186ec0&auto=format&fit=crop&w=500&q=60' },
-  { name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1503789597747-41de608aca69?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1e39a51d6a2893ca49d551ad22e86ccc&auto=format&fit=crop&w=500&q=60' },
-  { name: 'High Mountain', image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=500&q=60' },
-  { name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec456c4aeb71d3aecbe65e586d186ec0&auto=format&fit=crop&w=500&q=60' },
-  { name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1503789597747-41de608aca69?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1e39a51d6a2893ca49d551ad22e86ccc&auto=format&fit=crop&w=500&q=60' },
-  { name: 'High Mountain', image: 'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=500&q=60' }
-];
 
 app.get('/campgrounds', function(req, res) {
   //get campgrounds from the db
