77a365cdc2e81a6bc5bd0a8968e3cffc73d72742
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-05-28 15:01:57 +0200

    install flash messages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express'),
     passport = require('passport'),
     LocalStrategy = require('passport-local'),
     methodOverride = require('method-override'),
+    flash = require('connect-flash'),
     Campground = require('./models/campground'),
     Comment =  require('./models/comment'),
     User = require('./models/user'),
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
+app.use(flash());
 
 //Passport Configuration
 app.use(require('express-session')({
